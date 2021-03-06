3c4a38ebf8ddc60d467391a07a1a8f1a87f6fcf2
Brayan Potosi <61634825+BrayanPotosi@users.noreply.github.com>
2021-03-06 10:49:07 -0500

    Update script.js

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -6,20 +6,3 @@ BURGER_BUTTON.addEventListener('click', () => {
     ?MENU.classList.remove('is-active')
     :MENU.classList.add('is-active')
 })
-
-$('.carousel[data-type="multi"] .item').each(function(){
-    var next = $(this).next();
-    if (!next.length) {
-      next = $(this).siblings(':first');
-    }
-    next.children(':first-child').clone().appendTo($(this));
-  
-    for (var i=0;i<4;i++) {
-      next=next.next();
-      if (!next.length) {
-          next = $(this).siblings(':first');
-        }
-      
-      next.children(':first-child').clone().appendTo($(this));
-    }
-  });
