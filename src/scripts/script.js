a1f970e0bfb929c717ce3ff3e1402087e55fb553
Brayan Potosi <bp990308@hotmail.com>
2021-03-06 00:34:08 -0500

    Slide

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -6,3 +6,20 @@ BURGER_BUTTON.addEventListener('click', () => {
     ?MENU.classList.remove('is-active')
     :MENU.classList.add('is-active')
 })
+
+$('.carousel[data-type="multi"] .item').each(function(){
+    var next = $(this).next();
+    if (!next.length) {
+      next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+  
+    for (var i=0;i<4;i++) {
+      next=next.next();
+      if (!next.length) {
+          next = $(this).siblings(':first');
+        }
+      
+      next.children(':first-child').clone().appendTo($(this));
+    }
+  });
